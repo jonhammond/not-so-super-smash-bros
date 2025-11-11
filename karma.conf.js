70d9f3ab9663d65764f4725a8ddc5bd1439df7c7
Jon Hammond <jonh1016@gmail.com>
2025-11-11 13:19:03 -0700

    lab config for chromium

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,11 @@
 // karma.conf.js
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+// process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
-    browsers: ['ChromeHeadless'],
+    // browsers: ['ChromeHeadless'],
     singleRun: true,
     autoWatch: true,
     reporters: ['progress'],
