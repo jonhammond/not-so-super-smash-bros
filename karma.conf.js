a4f6e778cc9e521f8ab938992d41758bbb3b6720
Jon Hammond <jonh1016@gmail.com>
2025-11-11 14:02:49 -0700

    restored headless chrome config, updated flags

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,19 @@
 // karma.conf.js
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+// process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
     browsers: ['Chrome'],
+    customLaunchers: {
+      Headless_Chrome: {
+        base: "Chrome",
+        flags: ["--no-sandbox", "--disable-gpu"],
+      },
+    },
     singleRun: true,
-    // autoWatch: true,
+    autoWatch: true,
     reporters: ['progress'],
     files:["src/spec/**.js"]
   });
